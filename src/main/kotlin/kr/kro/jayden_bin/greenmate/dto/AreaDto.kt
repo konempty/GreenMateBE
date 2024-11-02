ab5e83b75632b323c5056be2f01a9b5108dbcc8d
jayden.bin <jayden.bin@kakaocorp.com>
2024-11-03 05:41:54 +0900

    배포 설정 추가

--- src/main/kotlin/kr/kro/jayden_bin/greenmate/dto/AreaDto.kt
+++ src/main/kotlin/kr/kro/jayden_bin/greenmate/dto/AreaDto.kt
@@ -4,7 +4,7 @@ import kr.kro.jayden_bin.greenmate.controller.team.request.AreaType
 import kr.kro.jayden_bin.greenmate.controller.team.request.Point
 
 data class AreaDto(
-    val type: AreaType,
+    val type: AreaType?,
     val center: Point?,
     val radius: Double?,
     val points: List<Point>?,
